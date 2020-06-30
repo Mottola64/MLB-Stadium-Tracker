006b8fbed42976ee0950d742dd9c6c77956d1b05
Michael Mottola <mottola64@gmail.com>
2020-06-29 23:05:03 -0400

    adding redux and store

--- src/App.js
+++ src/App.js
@@ -1,73 +1,102 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Home from './components/Home'
-import Login from './components/registrations/Login'
-import Signup from './components/registrations/Signup'
+import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/registrations/Login";
+import Signup from "./components/registrations/Signup";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let myStore = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       isLoggedIn: false,
-      user: {}
-     };
+      user: {},
+    };
   }
-componentDidMount() {
-    this.loginStatus()
-  }
-
-loginStatus = () => {
-    axios.get('http://localhost:3001/logged_in', {withCredentials: true})
-    .then(response => {
-      if (response.data.logged_in) {
-        this.handleLogin(response)
-      } else {
-        this.handleLogout()
-      }
-    })
-    .catch(error => console.log('api errors:', error))
+  componentDidMount() {
+    this.loginStatus();
   }
 
-handleLogin = (data) => {
+  loginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (response.data.logged_in) {
+          this.handleLogin(response);
+        } else {
+          this.handleLogout();
+        }
+      })
+      .catch((error) => console.log("api errors:", error));
+  };
+
+  handleLogin = (data) => {
     this.setState({
       isLoggedIn: true,
-      user: data.user
-    })
-  }
+      user: data.user,
+    });
+  };
 
-handleLogout = () => {
+  handleLogout = () => {
     this.setState({
-    isLoggedIn: false,
-    user: {}
-    })
-  }
+      isLoggedIn: false,
+      user: {},
+    });
+  };
 
-render() {
+  render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route 
-              exact path='/' 
-              render={props => (
-              <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
-              )}
-            />
-            <Route 
-              exact path='/login' 
-              render={props => (
-              <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
-              )}
-            />
-            <Route 
-              exact path='/signup' 
-              render={props => (
-              <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
-              )}
-            />
-          </Switch>
-        </BrowserRouter>
+        <Provider store={myStore} className="App">
+          <BrowserRouter>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Home
+                    {...props}
+                    handleLogout={this.handleLogout}
+                    loggedInStatus={this.state.isLoggedIn}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/login"
+                render={(props) => (
+                  <Login
+                    {...props}
+                    handleLogin={this.handleLogin}
+                    loggedInStatus={this.state.isLoggedIn}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/signup"
+                render={(props) => (
+                  <Signup
+                    {...props}
+                    handleLogin={this.handleLogin}
+                    loggedInStatus={this.state.isLoggedIn}
+                  />
+                )}
+              />
+            </Switch>
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
