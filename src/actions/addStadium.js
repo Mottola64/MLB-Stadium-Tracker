6367d4b3dc507968d46890a51a4188db3a5640ec
Michael Mottola <mottola64@gmail.com>
2020-06-29 23:00:54 -0400

    teams

---
+++ src/actions/addStadium.js
@@ -0,0 +1,5 @@
+export const addStadium = (data) => {
+    return (dispatch) => {
+        fetch("http://localhost:3000/api/v1/stadia")
+    }
+}
---
+++ src/components/Team.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Team extends React.Component {
+    render() {
+        return(
+            <div>Teams</div>
+        )
+    }
+}
+
+export default Team
